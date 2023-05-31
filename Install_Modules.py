cc6f9ba40eac0d143303d31159b382dbb24521ca
Hiro3328 <52054770+Hiro3328@users.noreply.github.com>
2023-05-31 19:35:16 -0300

    Delete Install_Modules.py

--- Install_Modules.py
+++
@@ -1,8 +0,0 @@
-import os
-#use the os lib to run 'pip install -r requirements.txt' in the terminal
-def install():
-    return os.system("pip install -r requirements.txt")
-
-def update():
-    return os.system("pip install --upgrade -r requirements.txt")
-
